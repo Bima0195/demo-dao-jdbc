412a40dfe1e356aca9612622dcda391d5e5acf57
Bima0195 <abimaelsouza2001@gmail.com>
2026-03-01 13:21:01 -0300

    [200~SellerDaoJDBC and DaoFactory~

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -2,14 +2,14 @@ package model.dao;
 
 import java.util.List;
 
-import model.entities.Department;
+import model.entities.Seller;
 
 public interface SellerDao {
 	
-	void insert(Department obj);
-	void update(Department obj);
-	void deleteById(Department obj);
-	Department findById(Integer id);
-	List<Department> findAll();
+	void insert(Seller seller);
+	void update(Seller seller);
+	void deleteById(Seller seller);
+	Seller findById(Integer id);
+	List<Seller> findAll();
 
 }
